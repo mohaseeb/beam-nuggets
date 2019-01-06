653dde0c3134462a4ecfd588cf3282e28e1dedad
Mohamed Haseeb <m@mohaseeb.com>
2019-01-06 02:11:08 +0100

    v0.7.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ REQUIRED_DOCUMENTATION = [
 
 setup(
     name='beam-nuggets',
-    version='0.6.0',
+    version='0.7.0',
     install_requires=[
         'apache-beam>=2.8.0,<3.0.0',
         'SQLAlchemy>=1.2.14,<2.0.0',
