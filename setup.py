39d2493b161ebbcbff9f4857115d527f6fefda77
Mohamed haseeb <m@mohaseeb.com>
2021-02-16 20:47:06 +0100

    v0.17.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from __future__ import division, print_function
 
 from setuptools import setup, find_packages
 
-VERSION = '0.17.0'
+VERSION = '0.17.1'
 
 REQUIRED_PACKAGES = [
     'apache-beam>=2.8.0,<3.0.0',
